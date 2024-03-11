017142ac6f6b7e206e094abb7950c9f98b39c97c
subhan khatri <subhankhatri922@gmail.com>
2024-03-11 12:44:00 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// User will add 2 number
+let num1 = prompt("Enter 1st value.");
+let num2 = prompt("Enter 2nd value.");
+
+// Allowing String to be shifted into number
+num1 = Number(num1);
+num2 = Number(num2);
+
+// Addition
+let sum = num1 + num2;
+alert("Addition: " + sum);
+console.log("Addition: " + sum);
+
+// Subtraction
+let minus = num1 - num2;
+alert("Subtraction: " + minus);
+console.log("Subtraction: " + minus);
+
+// Multiplication
+let multiply = num1 * num2;
+alert("Multiplication: " + multiply);
+console.log("Multiplication: " + multiply);
+
+// Division
+let divide = num1 / num2;
+alert("Division: " + divide);
+console.log("Division: " + divide);
